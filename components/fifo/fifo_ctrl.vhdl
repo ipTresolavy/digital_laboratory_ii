ff839a1ca50dc8f3a0ec03e2de1ddc480a15ee6e
Igor Pontes Tresolavy <tresolavy@usp.br>
2023-10-30 17:31:03 -0300

    melhora descrição do fifo_ctrl.vhdl

--- components/fifo/fifo_ctrl.vhdl
+++ components/fifo/fifo_ctrl.vhdl
@@ -23,81 +23,132 @@ end entity fifo_ctrl;
 
 architecture behavioral of fifo_ctrl is
 
+  component sync_par_counter is
+    generic
+    (
+      constant MODU : natural := 16
+    );
+    port (
+      clock  : in  std_logic;
+      reset  : in  std_logic;
+      cnt_en : in  std_logic;
+      q      : out std_logic_vector(natural(ceil(log2(real(MODU))))-1 downto 0)
+    );
+  end component sync_par_counter;
+
+  component register_d is
+    generic
+    (
+      WIDTH : natural := 8
+    );
+    port
+    (
+      clock         : in  std_logic;
+      reset         : in  std_logic;
+      enable        : in  std_logic;
+      data_in       : in  std_logic_vector(WIDTH-1 downto 0);
+      data_out      : out std_logic_vector(WIDTH-1 downto 0)
+    );
+  end component register_d;
+
+  -- ponteiro de escrita
+  signal w_ptr_logic_reset : std_logic;
+  signal w_ptr_logic_cnt_en : std_logic;
   signal w_ptr_logic : std_logic_vector(ADDR_WIDTH-1 downto 0);
-  signal w_ptr_next  : std_logic_vector(ADDR_WIDTH-1 downto 0);
-  signal w_ptr_succ  : std_logic_vector(ADDR_WIDTH-1 downto 0);
-  signal tmp_w_ptr_succ  : std_logic_vector(ADDR_WIDTH downto 0);
   
+  -- ponteiro de leitura
+  signal r_ptr_logic_reset : std_logic;
+  signal r_ptr_logic_cnt_en : std_logic;
   signal r_ptr_logic : std_logic_vector(ADDR_WIDTH-1 downto 0);
-  signal r_ptr_next  : std_logic_vector(ADDR_WIDTH-1 downto 0);
-  signal r_ptr_succ  : std_logic_vector(ADDR_WIDTH-1 downto 0);
-  signal tmp_r_ptr_succ  : std_logic_vector(ADDR_WIDTH downto 0);
+
+  -- registrador que guarda a última operação realizada
+  signal last_op_en  : std_logic;
+  signal last_op_out : std_logic_vector(1 downto 0);
 
   signal full_logic  : std_logic;
   signal empty_logic : std_logic;
-  signal full_next   : std_logic;
-  signal empty_next  : std_logic;
 
   signal wr_rd : std_logic_vector(1 downto 0);
 
 begin
 
+  w_ptr_logic_reset <= reset;
+  w_ptr_logic_counter: sync_par_counter
+  generic map
+  (
+    MODU => 2**ADDR_WIDTH
+  )
+  port map
+  (
+    clock => clock,
+    reset => w_ptr_logic_reset,
+    cnt_en => w_ptr_logic_cnt_en,
+    q => w_ptr_logic
+  );
+
+  r_ptr_logic_reset <= reset;
+  r_ptr_logic_counter: sync_par_counter
+  generic map
+  (
+    MODU => 2**ADDR_WIDTH
+  )
+  port map
+  (
+    clock => clock,
+    reset => r_ptr_logic_reset,
+    cnt_en => r_ptr_logic_cnt_en,
+    q => r_ptr_logic
+  );
+
+  last_op_reg: register_d
+  generic map
+  (
+    WIDTH => 2
+  )
+  port map
+  (
+    clock  => clock,
+    reset  => reset,
+    enable => last_op_en,
+    data_in => wr_rd,
+    data_out => last_op_out
+  );
+
   wr_rd <= wr & rd;
-  
-  fifo_control_logic: process(clock, reset)
-  begin
-    if reset = '1' then
-      w_ptr_logic <= (others => '0');
-      r_ptr_logic <= (others => '0');
-      full_logic  <= '0';
-      empty_logic <= '1';
-    elsif rising_edge(clock) then
-      w_ptr_logic <= w_ptr_next;
-      r_ptr_logic <= r_ptr_next;
-      full_logic  <= full_next;
-      empty_logic <= empty_next;
-    end if;
-  end process fifo_control_logic;
 
-  tmp_w_ptr_succ <= std_logic_vector(to_unsigned(to_integer(unsigned(w_ptr_logic)) + 1, tmp_w_ptr_succ'LENGTH));
-  tmp_r_ptr_succ <= std_logic_vector(to_unsigned(to_integer(unsigned(r_ptr_logic)) + 1, tmp_r_ptr_succ'LENGTH));
-
-  next_state_logic: process(w_ptr_logic, r_ptr_logic, full_logic, empty_logic, wr_rd, r_ptr_succ, w_ptr_succ, tmp_r_ptr_succ, tmp_w_ptr_succ)
+  next_state_logic: process(full_logic, empty_logic, wr_rd)
   begin
-    w_ptr_succ <= tmp_w_ptr_succ(w_ptr_succ'LENGTH-1 downto 0);
-    r_ptr_succ <= tmp_r_ptr_succ(r_ptr_succ'LENGTH-1 downto 0);
-    w_ptr_next <= w_ptr_logic;
-    r_ptr_next <= r_ptr_logic;
-    full_next  <= full_logic;
-    empty_next <= empty_logic;
+    r_ptr_logic_cnt_en <= '0';
+    w_ptr_logic_cnt_en <= '0';
+    last_op_en <= '0';
     
     case wr_rd is
       when "01" =>
         if empty_logic = '0' then
-          r_ptr_next <= r_ptr_succ;
-          full_next <= '0';
-          if r_ptr_succ = w_ptr_logic then
-            empty_next <= '1';
-          end if;
+          r_ptr_logic_cnt_en <= '1';
+          last_op_en <= '1';
         end if; 
 
       when "10" =>
         if full_logic = '0' then
-          w_ptr_next <= w_ptr_succ;
-          empty_next <= '0';
-          if w_ptr_succ = r_ptr_logic then
-            full_next <= '1';
-          end if;
+          w_ptr_logic_cnt_en <= '1';
+          last_op_en <= '1';
         end if;
 
       when "11" =>
-        w_ptr_next <= w_ptr_succ;
-        r_ptr_next <= r_ptr_succ;
+        w_ptr_logic_cnt_en <= '1';
+        r_ptr_logic_cnt_en <= '1';
 
       when others =>
 
     end case;
   end process next_state_logic;
+
+  full_logic  <= '1' when ((last_op_out = "10") and (w_ptr_logic = r_ptr_logic)) else
+                 '0';
+
+  empty_logic <= '1' when ((((last_op_out = "01") ) and (w_ptr_logic = r_ptr_logic)) or (last_op_out = "00"))else
+                 '0';
 
   w_addr <= w_ptr_logic;
   r_addr <= r_ptr_logic;
